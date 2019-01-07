17e22bd2eecc4c5ebd08b03a5710bf21407da468
johnvzeng <johnvzeng@tencent.com>
2019-01-07 19:58:22 +0800

    improve performance

--- main.py
+++ main.py
@@ -90,45 +90,52 @@ left_bound_for_left_img = left_bound(im)
 up_bound_for_left_img = up_bound(im)
 right_bound_for_left_img = right_bound(im)
 low_bound_for_left_img = low_bound(im)
-print_slice();
+print_slice()
 print("get left bound %d" % left_bound_for_left_img)
 print("get up bound %d" % up_bound_for_left_img)
-print_slice();
+print_slice()
 left_bound_for_right_img = left_bound(cmp_im)
 up_bound_for_right_img = up_bound(cmp_im)
-print_slice();
+print_slice()
 print("get left bound %d" % left_bound_for_right_img)
 print("get up bound %d" % up_bound_for_right_img)
 
+print_slice()
 
 def cmp(img1, img2):
+    (w1,h1) = img1.size
     (w,h) = img2.size
     img2_palette = img2.getpalette()
     img2_palette[10] = 0
     img2_palette[11] = 245
     img2_palette[12] = 255
+    print("palette setup done")
+    print_slice()
     
-    new_img = Image.new(img2.mode, (w,h))
+    new_img = img2.copy()
 
     new_img.putpalette(img2_palette)
-    for i in range(left_bound_for_left_img + 1, right_bound_for_left_img):
-        for j in range(up_bound_for_left_img + 1, low_bound_for_left_img):
+    complcate = (right_bound_for_left_img - left_bound_for_left_img - 1) * (low_bound_for_left_img - up_bound_for_left_img - 1)
+    print("complcate is %d" % complcate)
+    
+    i_range = range(left_bound_for_left_img + 1, right_bound_for_left_img)
+    j_range = range(up_bound_for_left_img + 1, low_bound_for_left_img)
+    img1Data = img1.getdata()
+    img2Data = img2.getdata()
+
+    for i in i_range:
+        for j in j_range:
             right_w = i - left_bound_for_left_img + left_bound_for_right_img
             right_h = j - up_bound_for_left_img + up_bound_for_right_img
-            if right_w >= w :
-                continue
+            if right_w >= w:
+                break
             if right_h >= h:
-                continue
-            delta = img1.getpixel((i,j)) - img2.getpixel((right_w,right_h))
-            new_img.putpixel((right_w, right_h), img2.getpixel((right_w,right_h)))
-#            print("[%d,%d] %d, [%d, %d] %d"% (i,j, img1.getpixel((i,j)), right_w, right_h, img2.getpixel((right_w, right_h))))
-            if abs(delta) >= 1:
+                break
+            img2Pix = img2Data[right_w+right_h * w]
+            delta = img1Data[i + j * w1] - img2Pix
+            if abs(delta) > 0:
                 new_img.putpixel((right_w, right_h), 3)
-#                print("too much diff at : (%d,%d) %d (%d %d ) %d "%(i,j, img1.getpixel((i,j)), right_w, right_h, img2.getpixel((right_w, right_h))))
     new_img.save("output.png")
 
-#cmp(shinked_im, shinked_im2)
-#shinked_im.save("new_image.png")
-#shinked_im2.save("new_image2.png")
 cmp(im, cmp_im)
 print("done")
