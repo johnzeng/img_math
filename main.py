ad8f79655e3498739b858f41c799430b4458be01
johnvzeng <johnvzeng@tencent.com>
2019-01-13 01:29:11 +0800

    use opecv

--- main.py
+++ main.py
@@ -1,141 +1,65 @@
-import os, sys
-from PIL import Image, ImageDraw
+import cv2 
+from matplotlib import pyplot as plt
+import Queue
 
-argv = sys.argv
-if len(argv) != 3:
-    print("usage: %s [left_img] [right_img]", argv[0])
-left = argv[1]
-right = argv[2]
+img = cv2.imread('e905c759-8b85-48ec-9855-18eeb6fcb002.jpg',0)
 
-im = Image.open(left)
-cmp_im = Image.open(right)
+cv2.imwrite("canny.jpg", cv2.Canny(img, 180, 200))
+img2 = cv2.imread("canny.jpg")
 
-resolution_ratio = 1
-print "checking resolution ratio %d" % resolution_ratio
-pixel_delta_range = 10
-print "checking pixel delta range %d" % pixel_delta_range
-limit = 1000
+lefti = 192
+leftj = 91
 
-palette = im.getpalette()
+righti = 377
+rightj = 696 
 
-def print_slice():
-    print("===================================")
+threshold = 12
 
-def right_bound(im):
-    print "checking right bound"
-    (weight, height) = im.size
-    pix = im.load()
-    for i in range(weight - 1, -1, -1):
-        flag = False
-        for j in range(height):
-            if pix[i,j] != 0 and pix[i,j]!=1:
-                flag = True
-#                print "get at %d %d ,color %d" % (i,j,pix[i,j])
+mark = dict()
+rect = dict()
+for i in range(lefti, righti + 1):
+    for j in range(leftj, rightj + 1):
+        R,G,B = img2[i,j]
+        #if R <= 200 and G <= 200 and B <= 200:
+        if R <= 20 and G <= 20 and B <= 20:
+            continue
+        if mark.has_key(i):
+            mark[i][j] = (i,j)
+        else:
+            mark[i] = dict()
+            mark[i][j] = (i,j)
+        #fine prev elements to check if there is something we can merge
+        for ti in range(-threshold,threshold + 1):
+            for tj in range(-threshold,threshold + 1):
+                curi = i + ti
+                curj = j + tj
+                if curi == i and curj == j:
+                    continue
+                if mark.has_key(curi) and mark[curi].has_key(curj):
+                    mark[i][j] = mark[curi][curj]
+                    break
+            nowi,nowj = mark[i][j]
+            if nowi != i or nowj != j:
                 break
-        if flag:
-            return i
-    print("nothing detected for left bound")
-    sys.exit(0)
 
-def low_bound(im):
-    print "checking low bound"
-    (weight, height) = im.size
-    pix = im.load()
-    for j in range(height - 1,  -1, -1):
-        flag = False
-        for i in range(weight):
-            if pix[i,j] != 0 and pix[i,j]!=1:
-                flag = True
-#                print "get at %d %d ,color %d" % (i,j,pix[i,j])
-                break
-        if flag:
-            return j
-    print("nothing detected for up bound")
-    sys.exit(0)
+        rooti, rootj = mark[i][j]
+        if rect.has_key(rooti) :
+            if rect[rooti].has_key(rootj):
+                mini,minj,maxi,maxj = rect[rooti][rootj]
+                rect[rooti][rootj] = (min(mini,i), min(minj,j), max(maxi,i), max(maxj,j))
+            else:
+                rect[rooti][rootj] = (i,j,i,j)
+        else:
+            rect[rooti] = dict()
+            rect[rooti][rootj] = (i,j,i,j)
 
-def left_bound(im):
-    print "checking left bound"
-    (weight, height) = im.size
-    pix = im.load()
-    for i in range(weight):
-        flag = False
-        for j in range(height):
-            if pix[i,j] != 0 and pix[i,j]!=1:
-                flag = True
-#                print "get at %d %d ,color %d" % (i,j,pix[i,j])
-                break
-        if flag:
-            return i
-    print("nothing detected for left bound")
-    sys.exit(0)
-
-def up_bound(im):
-    print "checking up bound"
-    (weight, height) = im.size
-    pix = im.load()
-    for j in range(height):
-        flag = False
-        for i in range(weight):
-            if pix[i,j] != 0 and pix[i,j]!=1:
-                flag = True
-#                print "get at %d %d ,color %d" % (i,j,pix[i,j])
-                break
-        if flag:
-            return j
-    print("nothing detected for up bound")
-    sys.exit(0)
+for i in rect.keys():
+    for j in rect[i].keys():
+        mini,minj,maxi,maxj = rect[i][j]
+        if (maxi - mini) * (maxj - minj) > 200:
+            cv2.rectangle(img2, (minj,mini), (maxj,maxi), (255,0,0), 2)
 
 
-left_bound_for_left_img = left_bound(im)
-up_bound_for_left_img = up_bound(im)
-right_bound_for_left_img = right_bound(im)
-low_bound_for_left_img = low_bound(im)
-print_slice()
-print("get left bound %d" % left_bound_for_left_img)
-print("get up bound %d" % up_bound_for_left_img)
-print_slice()
-left_bound_for_right_img = left_bound(cmp_im)
-up_bound_for_right_img = up_bound(cmp_im)
-print_slice()
-print("get left bound %d" % left_bound_for_right_img)
-print("get up bound %d" % up_bound_for_right_img)
 
-print_slice()
-
-def cmp(img1, img2):
-    (w1,h1) = img1.size
-    (w,h) = img2.size
-    img2_palette = img2.getpalette()
-    img2_palette[10] = 0
-    img2_palette[11] = 245
-    img2_palette[12] = 255
-    print("palette setup done")
-    print_slice()
-    
-    new_img = img2.copy()
-
-    new_img.putpalette(img2_palette)
-    complcate = (right_bound_for_left_img - left_bound_for_left_img - 1) * (low_bound_for_left_img - up_bound_for_left_img - 1)
-    print("complcate is %d" % complcate)
-    
-    i_range = range(left_bound_for_left_img + 1, right_bound_for_left_img)
-    j_range = range(up_bound_for_left_img + 1, low_bound_for_left_img)
-    img1Data = img1.getdata()
-    img2Data = img2.getdata()
-
-    for i in i_range:
-        for j in j_range:
-            right_w = i - left_bound_for_left_img + left_bound_for_right_img
-            right_h = j - up_bound_for_left_img + up_bound_for_right_img
-            if right_w >= w:
-                break
-            if right_h >= h:
-                break
-            img2Pix = img2Data[right_w+right_h * w]
-            delta = img1Data[i + j * w1] - img2Pix
-            if abs(delta) > 0:
-                new_img.putpixel((right_w, right_h), 3)
-    new_img.save("output.png")
-
-cmp(im, cmp_im)
-print("done")
+cv2.imwrite("canny_x.jpg", img2)
+plt.imshow(img2), plt.show()
